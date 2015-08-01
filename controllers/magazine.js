c3c38f941b157093dce5b5fd107d24d8c3c76d67
goku <manjunath@themediaant.com>
2015-08-02 05:02:50 +0530

    structural changes

--- controllers/magazine.js
+++ controllers/magazine.js
@@ -130,7 +130,7 @@ var Magazine = function()
         console.log(err); 
         res.status(200).json({err:err});  
       }
-      res.status(200).json({results});
+      res.status(200).json(results);
     });
   };
 
