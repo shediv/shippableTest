06aad5a4383a02805868f30e2bc5c6d96a917efa
shediv <shediv>
2015-08-05 18:36:30 +0530

    tmarecoomend API

--- routes/media.js
+++ routes/media.js
@@ -9,4 +9,6 @@ var MedCtrl = new (require('../controllers/media')).Med();
 //API :- http://localhost:3000/media
 router.post("/", function(req, res){MedCtrl.createMedia(req, res);});
 
+router.get("/underscore", function(req, res){MedCtrl.underscore(req, res);});
+
 module.exports = router;
