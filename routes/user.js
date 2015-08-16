dba7c163f4120678e6d42dcc6a78f7030ab83e3f
goku <manjunath@themediaant.com>
2015-08-17 00:01:08 +0530

    login

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var UserCtrl = new (require('../controllers/user')).User();
 
 router.post("/signup", function(req, res){UserCtrl.store(req, res);});
-router.post("/login", function(req, res){UserCtrl.authenticate(req, res);});
+router.post("/signin", function(req, res){UserCtrl.authenticate(req, res);});
 router.post("/getSession", function(req, res){UserCtrl.getSession(req, res);});
 
 module.exports = router;
