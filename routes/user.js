e975cf23bf89318071baab6dc474437de1e67fe1
goku <manjunath@themediaant.com>
2015-08-19 19:23:46 +0530

    user update

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ var router = express.Router();
 var UserCtrl = new (require('../controllers/user')).User();
 
 router.post("/signup", function(req, res){UserCtrl.store(req, res);});
+router.put("/", function(req, res){UserCtrl.update(req, res);});
 router.post("/signin", function(req, res){UserCtrl.authenticate(req, res);});
 router.post("/current", function(req, res){UserCtrl.getSession(req, res);});
 router.post("/logout", function(req, res){UserCtrl.logout(req, res);});
