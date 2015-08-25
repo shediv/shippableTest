9e54134ab04343dd0bf7ac6abbbcf5967e16896d
srujan reddy c <csrujanreddy@gmail.com>
2015-08-25 16:49:28 +0530

    getBestRates route renamed to /bestRates

--- routes/magazine.js
+++ routes/magazine.js
@@ -8,7 +8,7 @@ var MagCtrl = new (require('../controllers/magazine')).Mag();
 
 router.get("/", function(req, res){  MagCtrl.getMagazines(req, res); });
 router.get("/getFilters", function(req, res){ MagCtrl.getFilters(req, res); });
-router.get("/getBestRates", function(req, res){ MagCtrl.getFilters(req, res); });
+router.get("/bestRates", function(req, res){ MagCtrl.getFilters(req, res); });
 router.get("/compare", function(req, res){ MagCtrl.compare(req, res); });
 router.get("/related/:categoryId", function(req, res){ MagCtrl.relatedMedia(req, res) });
 router.get("/:urlSlug", function(req, res){ MagCtrl.show(req, res); });
