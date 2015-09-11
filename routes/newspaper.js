a607952ddd7bb5a4b43e27f327085d377d227689
goku <manjunath@themediaant.com>
2015-09-11 20:08:53 +0530

    routes optimization

--- routes/newspaper.js
+++ routes/newspaper.js
@@ -9,7 +9,6 @@ var NewspaperCtrl = new (require('../controllers/newspaper')).Newspaper();
 
 router.get("/", NewspaperCtrl.getNewspapers);
 router.get("/filters", NewspaperCtrl.getFilters);
-router.post("/bestRates", NewspaperCtrl.getBestRates);
 router.get("/compare", NewspaperCtrl.compare);
 router.get("/related/:categoryId", NewspaperCtrl.relatedMedia);
 router.get("/:urlSlug", NewspaperCtrl.show);
