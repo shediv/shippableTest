15a14bb3da1ee0c2c206e2f556b1b36c1026c650
goku <manjunath@themediaant.com>
2015-09-24 02:48:07 +0530

    optimizing unique views

--- libraries/common.js
+++ libraries/common.js
@@ -1,6 +1,7 @@
 var Common = function()
 {
 	var Media = require('../models/media').Media;
+	var TwelthCross = require('../models/12thCross').TwelthCross;
 	var UniqueVisitor = require('../models/uniqueVisitors').UniqueVisitor;
 	var Tools = require('../models/tool').Tools;
 	var Products = require('../models/product').Products;
@@ -19,28 +20,7 @@ var Common = function()
 			callback(err, categoryNames);
 		});
 	};
-
-	this.checkUniqueVisitor = function(user, callback) {
-		UniqueVisitor.findOne({remoteAddress: user.remoteAddress, urlSlug: user.urlSlug},
-          function(err, results)
-          {
-             if(results){
-              Media.update({urlSlug : user.urlSlug}, {$inc: { views: 1 }}, {upsert:true}, function(err, results){
-                    callback(err, results);
-              });
-             }
-             else{
-              Media.update({urlSlug : user.urlSlug}, {$inc: { views: 1, uniqueViews: 1 }}, {upsert:true}, function(err, results){
-                    var newVisitor = UniqueVisitor(user);
-                    newVisitor.save(function(err, newVisitor) {
-                     callback(err, newVisitor); 
-                    });                    
-              });
-             }    
-          }
-          );
-	};
-		
+	
 	this.removeHiddenAttributes = function(attributes){
 		for(key in attributes) {
 			if(attributes[key].hidden) delete attributes[key];
@@ -49,15 +29,17 @@ var Common = function()
 	};
 
 	this.uniqueVisits = function(visitor){
+		if(visitor.type == 'media') var model = new Media;
+		if(visitor.type == '12thcross') var model = new TwelthCross;
 		UniqueVisitor.findOne(visitor).lean().exec(function(err, log){
 			if(log)
 			{
-				Media.update({ urlSlug:visitor.urlSlug }, { $inc:{ views:1 } }, { upsert:true });
-				UniqueVisitor.update(visitor, { $inc:{ views:1 } }, { upsert:true });
+				Media.update({ urlSlug:visitor.urlSlug }, { $inc:{ views:1 } }, { upsert:true }).exec();
+				UniqueVisitor.update(visitor, { $inc:{ views:1 } }, { upsert:true }).exec();
 			}
 			else
 			{
-				Media.update({ urlSlug:visitor.urlSlug }, { $inc:{ views:1, uniqueViews:1 } }, { upsert:true });
+				Media.update({ urlSlug:visitor.urlSlug }, { $inc:{ views:1, uniqueViews:1 } }, { upsert:true }).exec();
 				visitor.views = 1;
 				var newVisitor = UniqueVisitor(visitor);
 				newVisitor.save();
