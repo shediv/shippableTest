4dc436c7c8c94c5df3be706c14438c007178d0e1
shediv <shediv>
2015-10-05 15:46:17 +0530

    local app.js added for lsquare

--- controllers/lsquare.js
+++ controllers/lsquare.js
@@ -9,15 +9,15 @@ var Lsquare = function()
   var Category = require('../models/category').Category;
   
   this.params = {};
-  this.toolName = "lsquare";
+  this.toolName = "digital";
   var self = this;
 
   Tools.findOne({name: this.toolName}, function(err, result){
     self.toolId = result._id.toString();
   });
 
-  this.getDigital = function(req, res){
-    self.params = JSON.parse(req.query.params);
+  this.getLsquare = function(req, res){
+    //self.params = JSON.parse(req.query.params);
     res.status(200).json("result");
     async.waterfall([
       function(callback)
@@ -61,7 +61,7 @@ var Lsquare = function()
       async.parallel({
         count : function(callbackInner)
         {          
-          Lsquare.aggregate(
+          Media.aggregate(
             {$match : query.match},
             {$group: { _id : null, count: {$sum: 1} }},
             function(err, result)
@@ -82,28 +82,33 @@ var Lsquare = function()
           query.sortBy._id = 1;
           Media.aggregate(
             {$match: query.match}, {$sort: query.sortBy},
-            //{$skip : query.offset}, {$limit: query.limit},
+            {$skip : query.offset}, {$limit: query.limit},
             {$project: query.projection}, 
             function(err, results) 
             {
+              var questionUserIds = [];
+              for(i in results) { questionUserIds.push(results[i].userId); }
+              CommonLib.getUserInfo(questionUserIds, function(err, userInfo){
+                for(i in results)
+                  results[i].aksedBy = userInfo[results[i].aksedBy];
+                callbackInner(err, results);
+              });                  
+
                callbackInner(err, results);
             }
           );
         }
       },
       function(err, results) 
-      {
+      {        
         callback(err, results);
       });
     };
 
   this.getFilters = function(req, res){
     async.parallel({
-      categories : self.getTrendingQuestions,
-      // mediums : self.getMediums,
-      // geoTargets : self.getGeoTargets,
-      // pricingModels : self.getPricingModels,
-      // languages : self.getLanguages
+      trendingQuestions : self.getTrendingQuestions,
+      topTags : self.getTopTags
     },
     function(err, results) 
     {
@@ -113,46 +118,69 @@ var Lsquare = function()
   };
 
     self.getTrendingQuestions = function(callback){
-      Lsquare.aggregate(
+      Media.aggregate(
         {$match: {isActive : 1}},
-        {$sort: {"score": -1, "views": -1} }
-        {$group : { _id : '$question', count : {$sum : 1}}},
+        {$sort: {"score": -1, "views": -1}},
         function(error, results) 
         {
+          for(i in results) {
+            results['noOfAnswers'] = results[i].answer.length; 
+          }
+
+          var userIds = [];
+          results.map(function(o){ userIds.push(o.userId); });
+
           callback(error, results);
         }
       );
+    };
+
+    self.getTopTags = function(callback){
+        Media.aggregate(
+          {$match: {isActive : 1}},
+          {$sort: {"score": -1, "views": -1} },        
+          function(error, results) 
+          {
+            var questionIds = [];
+            results.map(function(o){ questionIds.push(o._id); });
+            Category.find({_id : {$in: questionIds}},'name').lean().exec(function(err, tags){
+              callback(error, tags);
+            });
+          }
+        );
     };
 
   this.addQuestion = function(req, res){
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
       if(!token) return res.status(401).json("Token not found");
       jwt.verify(token, self.config.secret, function(err, decoded){
-      if(err) res.status(401).json("Invalid Token");
-      else {
-        var url = req.body.question;
-        // convert spaces to '-'
-        url = url.replace(/ /g, "-");
-        // Make lowercase
-        url = url.toLowerCase();
-        // Remove characters that are not alphanumeric or a '-'
-        url = url.replace(/[^a-z0-9-]/g, "");
-        // Combine multiple dashes (i.e., '---') into one dash '-'.
-        url = url.replace(/[-]+/g, "-");
-        var question: {
-            url: req.body.userId,
-            title : req.body.question,
-            description : req.body.description,
-            title: title
-          }
-        var newQuestion = Lsquare(question);
+        if(err) res.status(401).json("Invalid Token");
+        else {
+          var question = {};
+          var url = req.body.question;
+          // convert spaces to '-'
+          url = url.replace(/ /g, "-");
+          // Make lowercase
+          url = url.toLowerCase();
+          // Remove characters that are not alphanumeric or a '-'
+          url = url.replace(/[^a-z0-9-]/g, "");
+          // Combine multiple dashes (i.e., '---') into one dash '-'.
+          url = url.replace(/[-]+/g, "-");
 
-        newQuestion.save(function(err){
-          if(err) return res.status(500).json(err);
-          res.status(200).json({newQuestion:newQuestion._id});
-        }
+          question.aksedBy = req.body.userId;
+          question.url = url;
+          question.title = req.body.question;
+          question.description = req.body.description;
+          question.title = req.body.title;
+          question.tags = req.body.tags;  
 
-      }
+          var newQuestion = Lsquare(question);
+
+          newQuestion.save(function(err){
+            if(err) return res.status(500).json(err);
+            res.status(200).json({newQuestion:newQuestion._id});
+          });
+        }      
     });
   };
 
@@ -160,23 +188,41 @@ var Lsquare = function()
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
       if(!token) return res.status(401).json("Token not found");
       jwt.verify(token, self.config.secret, function(err, decoded){
-      if(err) res.status(401).json("Invalid Token");
-      else {
-        var answer: {
-            url: req.body.userId,
-            answer : req.body.answer,
-            description : req.body.description,
-            id: questionID
-          }        
+        if(err) res.status(401).json("Invalid Token");
+        else {
+          var answer = {};
+          answer.answeredBy = req.body.userId;
+          answer.answer = req.body.answer;
+          answer.description = req.body.description;
+          answer.id = questionID;        
 
-        Lsquare.findOneAndUpdate(query, answer, {upsert:true}, function(err, doc){
-          if (err) return res.send(500, { error: err });
-          return res.status(200).json("succesfully updated");
-        });
-
-      }
+          Lsquare.findOneAndUpdate({_id: answer.id}, answer, {upsert:true}, function(err, doc){
+            if (err) return res.send(500, { error: err });
+            return res.status(200).json("succesfully updated");
+          });
+        }
     });
   };  
+
+  this.upvoteAnswer = function(req, res){
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+      if(!token) return res.status(401).json("Token not found");
+      jwt.verify(token, self.config.secret, function(err, decoded){
+        if(err) res.status(401).json("Invalid Token");
+        else {
+          var answer = {};
+          answer.answeredBy = req.body.userId;
+          answer.answer = req.body.answer;
+          answer.description = req.body.description;
+          answer.id = questionID;        
+
+          Lsquare.findOneAndUpdate({_id: answer.id}, answer, {upsert:true}, function(err, doc){
+            if (err) return res.send(500, { error: err });
+            return res.status(200).json("succesfully updated");
+          });
+        }
+    });
+  };
 
   this.show = function(req, res){
     Lsquare.findOne({urlSlug: req.params.urlSlug}).lean().exec(function(err, results){
@@ -193,60 +239,6 @@ var Lsquare = function()
       tool: self.toolName
     };
     CommonLib.uniqueVisits(visitor);
-  };
-
-  this.compare = function(req, res){
-    var ids = JSON.parse(req.query.params);
-    var catIds = [];
-    var project = {
-      '_id' : 1,
-      'urlSlug' : 1,
-      'name' : 1,
-      'medium' : 1,
-      'mediaOptions' : 1,
-      'language' : 1,        
-      'logo' : 1,
-      'geoTagging' : 1,
-      'reach1' : 1,
-      'reach2' : 1,
-      'unit1' : 1,
-      'unit2' : 1,
-      'categoryId' : 1
-    };
-    
-    Media.find({_id: { $in: ids }}, project).lean().exec(function(err, results){
-      if(err) return res.status(500).json(err);
-      async.each(results, function(result, callback){
-        if(result['reach1'] !== undefined && result['unit1'])
-          result['reach1'] = result['reach1'] + ' ' + result['unit1'];
-        if(result['reach2'] !== undefined && result['unit2'])
-          result['reach2'] = result['reach2'] + ' ' + result['unit2'];
-        
-        firstmediaOptionsKey = Object.keys(result['mediaOptions'])[0];
-        if(result.mediaOptions[firstmediaOptionsKey].minimumQtyUnit1 === undefined){ minimumQtyUnit1 = false;} else { minimumQtyUnit1 = result.mediaOptions[firstmediaOptionsKey].minimumQtyUnit1; }
-        if(result.mediaOptions[firstmediaOptionsKey].minimumQtyUnit2 === undefined){ minimumQtyUnit2 = false;} else { minimumQtyUnit2 = result.mediaOptions[firstmediaOptionsKey].minimumQtyUnit2; }
-        if(result.mediaOptions[firstmediaOptionsKey].pricingUnit1 === undefined){ pricingUnit1 = false;} else { pricingUnit1 = result.mediaOptions[firstmediaOptionsKey].pricingUnit1; }
-        if(result.mediaOptions[firstmediaOptionsKey].pricingUnit2 === undefined){ pricingUnit2 = false;} else { pricingUnit2 = result.mediaOptions[firstmediaOptionsKey].pricingUnit2; }                                      
-        
-        if(minimumQtyUnit2)
-        {
-          minimumUnit = minimumQtyUnit1 + ' ' + pricingUnit1 + ' / ' + minimumQtyUnit2 + ' ' + pricingUnit2;
-          minimumBilling = (result.mediaOptions[firstmediaOptionsKey].cardRate * minimumQtyUnit1 * minimumQtyUnit2);
-        }
-        else
-        {
-          minimumUnit =  minimumQtyUnit1 + ' ' +  pricingUnit1;
-          minimumBilling =  result.mediaOptions[firstmediaOptionsKey].cardRate *  minimumQtyUnit1;
-        }
-        result['minimumBilling'] = minimumBilling;
-        Category.findOne({ _id:result.categoryId },'name').lean().exec(function(err, cat){
-          if(cat) result.categoryName = cat.name;
-          callback(err);
-        });
-      }, function(err){
-        res.status(200).json({medias:results});  
-      });
-    });
   };
 };
 
