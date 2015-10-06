aadb5e89787fa36b50a7f64795e4d211f63fc813
harishhub_8310 <hk8049@gmail.com>
2015-10-06 19:12:44 +0530

    added files for project in libraries

---
+++ libraries/relatedProject.js
@@ -0,0 +1,36 @@
+var relatedProject ={
+
+	"magazine" : {
+	  'urlSlug' : 1,
+    'name': 1,
+    'thumbnail' : 1,
+    'attributes' : 1,
+    'categoryId' : 1,
+    '_id' : 1,
+    'logo': 1,
+    'print.mediaOptions.fullPage.1-2' : 1
+	},
+
+	"newspaper" : {
+		'_id' : 1,
+    'urlSlug' : 1,
+    'name'       : 1,
+    'editionName' : 1,
+    'circulation' : 1,
+    'areaCovered' : 1,
+    'language' : 1,
+    'urlSlug' : 1,
+    'mediaOptions.anyPage.<800SqCms.cardRate' : 1,        
+    'logo' : 1
+	},
+
+	"radio": {
+		'_id' : 1,
+    'radioFrequency' : 1,
+    'station' : 1,
+    'geography' : 1,
+    'language' : 1,
+    'mediaOptions.regularOptions' : 1,        
+    'logo' : 1
+	}
+} 
