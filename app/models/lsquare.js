7e330d58a45006741a65356aa32c60623d83b94a
harishhub_8310 <hk8049@gmail.com>
2015-10-23 13:12:13 +0530

    data fix in lsquare

--- app/models/lsquare.js
+++ app/models/lsquare.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 
 var lsquare = mongoose.model('lsquare', new mongoose.Schema({},{strict : false}), 'lsquares');
 
-module.exports = { Lsquare : lsquare};
+module.exports = { Lsquare : lsquare };
 
