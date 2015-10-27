8a1cdc96f6bdd8c74b3c8e7c5366da9941f02d11
harishhub_8310 <hk8049@gmail.com>
2015-10-27 11:01:56 +0530

    removed app/models/lsquareAnswers.js

--- app/models/lsquareAnswers.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-var lsquareAnswers = mongoose.model('lsquareAnswers', new mongoose.Schema({},{strict : false}), 'lsquareAnswers');
-
-module.exports = { LsquareAnswers : lsquareAnswers};
