484ec47d619e83176811ed8797f7d5734a1d37ab
shediv <shediv>
2016-04-04 18:03:09 +0530

    Logic added

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -24,9 +24,45 @@ var User = function()
 	this.check = function(req, res){
 		var ghrepo = client.repo(req.query.urlSlug);
 		ghrepo.issues(function (err, status, body, headers) {
-  		return res.status(200).json({status : status, count : status.length})
+  		//return res.status(200).json({status : status, count : status.length})
+  		var last24hoursData = [];
+  		var last7daysData = [];
+  		var above7daysData = [];
+  		for(i in status){
+  			if(self.last24hours(status[i].created_at)){
+  				last24hoursData = last24hoursData.concat(status[i])
+  			}
+  			else if(self.last7days(status[i].created_at)){
+  				last7daysData = last7daysData.concat(status[i])
+  			}
+  			else if(self.above7days(status[i].created_at)){
+  				above7daysData = above7daysData.concat(status[i])
+  			}
+  		}
+
+			return res.status(200).json({last24hoursData : last24hoursData, last24hoursDataCount : last24hoursData.length, last7daysData : last7daysData, last7daysDataCount : last7daysData.length, above7daysData : above7daysData, above7daysDataCount : above7daysData.length, count : status.length})  		
 		});	
 	};
+
+	this.last24hours = function(datetime){
+		var before = new Date(datetime),
+    now = new Date();
+    return ( ( now - before ) < ( 1000 * 60 * 60 * 24 )  ) ? true : false;
+	}
+
+	this.last7days = function(datetime){
+		var before = new Date(datetime),
+    now = new Date();
+    return ( ( now - before ) < ( 1000 * 60 * 60 * 24 * 7)  ) ? true : false;
+	}
+
+	this.above7days = function(datetime){
+		var before = new Date(datetime),
+    now = new Date();
+    return ( ( now - before ) > ( 1000 * 60 * 60 * 24 * 7)  ) ? true : false;
+	}
+
+
 }
 
 module.exports.User = User;
