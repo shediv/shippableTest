9dfa5d32b92be47c68a4858b98f2c7e0ecf6b838
shediv <shediv>
2016-04-04 17:49:27 +0530

    add octane

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var path = require('path');
 //var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var cors = require('express-cors');
-var multer = require('multer');
 var jwt = require('jsonwebtoken');
 
 var config = require('./app/config/config.js');
@@ -21,44 +19,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
-app.use(cors({
-    allowedOrigins: [
-        'http://tma.dev',
-        'http://themediaant.com',
-        'http://www.themediaant.com',
-        'http://testing.themediaant.com',
-        'http://www.m20.in',
-        'http://m20.in',
-        'http://tma.dev:3000',
-        'http://www.tma.dev:3000',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000'
-    ],
-	headers: [
-		'x-access-token', 'Content-Type'
-	]
-}));
-
-mongoose.connect(envConfig.mongoUrl, function(err){
-  if(err) mongooseLog('Mongoose error: ' + err);
-});
-
-//MONGODB CONNECTION EVENTS.
-mongoose.connection
-    .on('connected', function () {
-        mongooseLog('Connection open to ' + envConfig.mongoUrl);
-    })
-    .on('error',function (err) {
-        mongooseLog('Connection error: ' + err);
-    })
-    .on('disconnected', function () {
-        mongooseLog('Connection disconnected');
-    });
-
-function mongooseLog(data) {
-  return console.log(data);
-}
-
 app.use(function(req, res, next){
   console.log(  "\033[34m \033[1m" + req.method , 
                 "\033[36m \033[1m REQUEST URL: " + "\033[32m "+req.url , 
@@ -72,29 +32,6 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(multer({dest: './public/temp/'}).single('file'));
-
-// check if login required
-// app.use(function(req, res, next) {
-//   RoutesCollection.findOne({url:req.url, isAuthReq:true}).lean().exec(
-//     function(err, result)
-//     {
-//       if(err) return res.status(500).json(err);
-//       if(result){
-//         var token = req.body.token || req.query.token || req.headers['x-access-token'];
-//         if(!token) return res.status(401).json("Token not found");
-//         jwt.verify(token, config.secret, function(tokenErr, decoded){
-//           if(tokenErr) return res.status(401).json("Invalid Token");
-//           else next();
-//         });
-//       }
-//       else{
-//          next();
-//       }
-//     }
-//   );
-// });
 
  
 app.use('/user', user);
